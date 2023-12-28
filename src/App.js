db18f8658619bc2077a350f110a85a5d6094208e
calebosam <caleb.osam@amalitech.com>
2023-12-28 17:24:52 +0000

    fix: resolve text-align issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
