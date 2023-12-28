5eb4b7f5f3dc8b189af6f3dfe5e542a4add1851e
calebosam <caleb.osam@amalitech.com>
2023-12-28 15:37:29 +0000

    feat: update user dashboard with

---
+++ src/components/UserCard/UserCard.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+
+const UserCard = (props) => {
+  const users = props.users
+  return (
+    <div>
+      <Container sx={{ py: 8 }} maxWidth="md">
+        <Grid container spacing={4}>
+          {users.map((user) => (
+            <Grid item key={user.id} xs={12} sm={6} md={4}>
+              <Card
+                sx={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <CardMedia
+                  component="div"
+                  sx={{
+                    // 16:9
+                    pt: "56.25%",
+                  }}
+                  image="https://source.unsplash.com/random?wallpapers"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {user.firstName}
+                  </Typography>
+                  <Typography>
+                    My name is {user.firstName} {user.lastName} with email{" "}
+                    {user.email} and my password is {user.password}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">View</Button>
+                  <Button size="small">Edit</Button>
+                  <Button size="small">Delete</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+export default UserCard;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,16 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Stack,Button } from "@mui/material";
+import UserCard from "../components/UserCard/UserCard";
+import { BASE_URL } from "../config";
+import axios from 'axios'
+
+const systemUsers = [
+  {
+    id: "1",
+    firstName: "Caleb",
+    lastName: "Osam",
+    email: "email@gmail.com",
+    password: "password",
+  },
+  {
+      id: "2",
+      firstName: "Gabby",
+      lastName: "Maru",
+      email: "gabby@gmail.com",
+      password: "password",
+    },
+];
 
 const Dashboard = () => {
+  const [externalUsers, setExternalUsers] = useState([])
+  const [externalSrc, setExternalSrc] = useState("hello")
+
+  const handleDataFetch = async ()=> {
+    const {data} = await axios.get(`${BASE_URL}/api/v1/externalUsers`)
+    console.log(data)
+    setExternalSrc(data.src)
+    setExternalUsers(data.data)
+  }
+
+  const handleClearCache = async ()=> {
+    await axios.delete(`${BASE_URL}/api/v1/externalUsers`)
+  }
+  useEffect( ()=>{
+    handleDataFetch()
+  }, [])
   return (
-    <div>
+    <>
+      <div>
         <h1>User Dashboard</h1>
         <p>id: {localStorage.getItem("id")}</p>
         <p>First Name: {localStorage.getItem("firstName")}</p>
         <p>Last Name: {localStorage.getItem("lastName")}</p>
         <p>Email: {localStorage.getItem("email")}</p>
         <p>Password: {localStorage.getItem("password")}</p>
-    </div>
-  )
-}
+      </div>
+      <div>
+        <h1>System Users</h1>
+            <UserCard users={systemUsers}/>
+      </div>
+      <div>
+      
+        <h1>External Users</h1>
+        <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained" onClick={() => handleDataFetch()}>Fetch Data</Button>
+              <Button variant="outlined" onClick={() => handleClearCache()}>Clear Cache</Button>
+            </Stack>
+            <h2>Data Source: {externalSrc}</h2>
+            <UserCard users={externalUsers} />
+      </div>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
